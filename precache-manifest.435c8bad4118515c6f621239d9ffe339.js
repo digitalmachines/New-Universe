30b2816f8faf066a8160ab3f511a9a514254f14c
john <jgreenberg@runbox.com>
2021-10-22 17:53:54 -0400

    Updates

---
+++ precache-manifest.435c8bad4118515c6f621239d9ffe339.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2a730ce40f0c9a6c155b5fa7028807",
+    "url": "/New-Universe/index.html"
+  },
+  {
+    "revision": "fceb94ecf93097428211",
+    "url": "/New-Universe/static/css/main.bb78ce5d.chunk.css"
+  },
+  {
+    "revision": "9d3f7d71f0a2a693e893",
+    "url": "/New-Universe/static/js/2.17622420.chunk.js"
+  },
+  {
+    "revision": "fceb94ecf93097428211",
+    "url": "/New-Universe/static/js/main.63154a3a.chunk.js"
+  },
+  {
+    "revision": "9114e3bb12989692d88f",
+    "url": "/New-Universe/static/js/runtime~main.8c04d6f2.js"
+  },
+  {
+    "revision": "0dc9842e46dfb5889d5157b93f28f740",
+    "url": "/New-Universe/static/media/nasa-footer.0dc9842e.png"
+  },
+  {
+    "revision": "490cec76253e10e97b3cf4cbd5174d78",
+    "url": "/New-Universe/static/media/nasa-orion-header.490cec76.jpg"
+  }
+]);
